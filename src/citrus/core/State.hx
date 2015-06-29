331921da96504b534fa1256fe95e26f13ae1e315
Aymeric <aymeric.lamboley@gmail.com>
2015-06-29 15:20:47 +0200

    state

---
+++ src/citrus/core/State.hx
@@ -0,0 +1,16 @@
+package citrus.core;
+
+import pixi.core.display.Container;
+
+class State extends Container {
+	
+	var _citrusJS:CitrusJS;
+
+	public function new() {
+		super();
+	}
+	
+	function onUpdate(elapsedTime:Float) {
+		
+	}
+}
